d9449a969dc3b8ab6927e9e0ffa7970325b91709
Kelvin Ludwig <kelvinludwig@roadseg.com.br>
2021-07-18 17:37:59 -0300

    Configuring porto to Heroku

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const server = require('./server')
 //Start the HTTP service listening in port 8080
 console.log('Running service...')
 const sv = http.createServer(server.requestListener)
-sv.listen(8080)
+sv.listen(process.env.PORT || 8080)
